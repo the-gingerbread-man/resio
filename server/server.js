d260e3f2ce2ac05c1a457eee02f6eb8835be0b93
Dave <david.loyst@gmail.com>
2016-04-15 12:15:30 -0700

    Merge pull request #12 from dloyst/master
    removed the test html file
    LGTA

--- server/server.js
+++ server/server.js
@@ -3,15 +3,15 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const reactEngine = require('react-engine');
-var io = require('socket.io')(app);
-
 var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
 
 app.listen(3000);
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
   function (err, data) {
+  	console.log(err);
     if (err) {
       res.writeHead(500);
       return res.end('Error loading index.html');
@@ -25,11 +25,10 @@ function handler (req, res) {
 io.on('connection', function(socket){
   console.log('a user connected');
 
-  socket.on('viewerAnswer', function(data) {
+  socket.on('Response', function(data) {
   	//Send data to presenter
-  	socket.emit('newResponse', function(data) {
+  	socket.emit('responseDate', data);
 
-  	})
   })
 
   socket.on('disconnect', function(){
