22ac19f35275e4cacc8ea611a6abf3d843ff77ac
Dave <david.loyst@gmail.com>
2016-04-18 11:45:59 -0700

    final version (#42)
    * Server Running
    
    * changed index placement
    
    * resolved conflicts
    
    * fixed server
    
    * new changes
    
    * fixed testing suite
    
    * hold on
    
    * sockets working
    
    * voting version 1.0
    
    * Graph working
    
    * Pie Chart Working
    
    * conflicts
    
    * graph generator and sockets
    
    * conflicts
    
    * MVP
    
    * presented version
    
    * final version

--- server/server.js
+++ server/server.js
@@ -31,6 +31,8 @@ app.get('/client/bundle.js', (req, res) => {
 //'.on' acts as an event listener - socket will only listen for events
 //while only io will emit events with the '.emit' method
 //this is different in the react components where socket will both listen and emit
+//this particular event 'viewerAnswer' is being emitted from the viewer at Viewer-Choice.jsx
+//then the server emits the data via the 'serverResponse' event, which is heard by Presenter-Dashboard.jsx
 io.on('connection', socket => {
   socket.on('viewerAnswer', data => {
     io.emit('serverResponse', data);
@@ -41,6 +43,15 @@ app.get('/api/questions', (req, res) => {
   res.json(database);
 });
 
+//data is hard coded into the server for now
+//for the app to work, the data structure should be formatted as follows:
+//an object containing one key named 'questions' whose value is an array containing all questions
+//each element in the array is an object containing 4 keys
+//'cType' will determine graph type in Presenter-IndividualGraph.jsx
+//'question' is just the question text that will be displayed
+//'choices' is an array of all possible choices - these will also be displayed
+//'qType' is used to determine whether the thumb graphic is used in Viewer-Choices.jsx
+//both Presenter-Dashboard.jsx and Viewer-QuestionApp.jsx make an ajax request to grab this data
 var database = {
   questions:
   [
--- src/components/Presenter-Dashboard.jsx
+++ src/components/Presenter-Dashboard.jsx
@@ -8,8 +8,9 @@ class Dashboard extends React.Component{
     super(props);
     this.state = {questions: []}
 
-    //needed to retain the value of this because it's within the socket callback
+    //needed to retain the value of 'this' because it's within the socket callback
     //we change a value in the 'choices' array based on an event called 'serverResponse'
+    //'serverResponse' is being emitted from the server
     //need to JSON parse the data coming from the socket
     //then use that data to traverse the data structure and change the state
     //see server.js for the structure of the data
